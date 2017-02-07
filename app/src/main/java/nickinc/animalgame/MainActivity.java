ada8bbb6deb8e685e5493e806ffd9f19eea73601
Nick Stabile <ncstabile17@gmail.com>
2017-02-07 10:25:19 -0500

    Layout, movement working; overlap not working

--- app/src/main/java/nickinc/animalgame/MainActivity.java
+++ app/src/main/java/nickinc/animalgame/MainActivity.java
@@ -1,9 +1,14 @@
 package nickinc.animalgame;
 
+import android.content.DialogInterface;
+import android.graphics.Rect;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -12,8 +17,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView pos1, pos2, pos3, pos4, pos5, pos6, pos7;
+    ImageView pos1, pos2, pos3, pos4, pos5, pos6, pos7, playerImage;
+    ImageButton leftButton, rightButton;
     ArrayList<ImageView> pos = new ArrayList<>();
+    Animation animation;
+    Rect myViewRect, otherViewRect1, otherViewRect2, otherViewRect3, otherViewRect4, otherViewRect5, otherViewRect6, otherViewRect7;
 
 
     @Override
@@ -29,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         pos6 = (ImageView) findViewById(R.id.imageView6);
         pos7 = (ImageView) findViewById(R.id.imageView7);
 
+        leftButton = (ImageButton) findViewById(R.id.leftButton);
+        rightButton = (ImageButton) findViewById(R.id.rightButton);
+
+        playerImage = (ImageView) findViewById(R.id.playerImage);
+
         pos.add(pos1);
         pos.add(pos2);
         pos.add(pos3);
@@ -37,46 +50,144 @@ public class MainActivity extends AppCompatActivity {
         pos.add(pos6);
         pos.add(pos7);
 
-        start();
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveLeft();
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveRight();
+            }
+        });
+
+
+        myViewRect = new Rect();
+        playerImage.getHitRect(myViewRect);
+
+        otherViewRect1 = new Rect();
+        pos1.getHitRect(otherViewRect1);
+
+        otherViewRect2 = new Rect();
+        pos2.getHitRect(otherViewRect2);
+
+        otherViewRect3 = new Rect();
+        pos3.getHitRect(otherViewRect3);
+
+        otherViewRect4 = new Rect();
+        pos4.getHitRect(otherViewRect4);
+
+        otherViewRect5 = new Rect();
+        pos5.getHitRect(otherViewRect5);
+
+        otherViewRect6 = new Rect();
+        pos6.getHitRect(otherViewRect6);
+
+        otherViewRect7 = new Rect();
+        pos7.getHitRect(otherViewRect7);
+
+        dialog();
+
+
     }//onCreate
 
     public void start() {
 
-//        ImageView curView = pos.get(1);
-//        curView.setImageResource(R.drawable.crab);
-//        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanim);
-//        animation.setRepeatCount(Animation.INFINITE);
-//        curView.startAnimation(animation);
-//
-//        ImageView curView1 = pos.get(2);
-//        curView1.setImageResource(R.drawable.crab);
-//        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanim);
-//        animation1.setStartOffset(4000);
-//        curView1.startAnimation(animation1);
+
 
         long offset = 1000;
         int bool = 0;
     int i = 0;
-        while (i<10) {
+        while (i<pos.size()) {
             Random rand = new Random();
-            ImageView curView = pos.get(rand.nextInt(pos.size()));
-            bool = rand.nextInt(2);
-            if (bool == 0) {
-                curView.setImageResource(R.drawable.crab);
-            }
-            else {
-                curView.setImageResource(R.drawable.platypus);
-            }
-            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanim);
+            ImageView curView = pos.get(i);
+            curView.setImageResource(R.drawable.raindrop);
+            animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanim);
+
+
             animation.setStartOffset(offset);
-          //  animation.setRepeatCount(Animation.INFINITE);
+            //animation.setRepeatCount(Animation.INFINITE);
             curView.startAnimation(animation);
 
             offset= (int) (offset + (rand.nextDouble() * 1000));
             i++;
-            animation.cancel();
+
+
         }
 
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation anim) {
+              //  overlap(animation);
+                while(!(animation.hasEnded())) {
+                   // Toast.makeText(getApplicationContext(), "look it's toast", Toast.LENGTH_LONG).show();
+
+//                    if (Rect.intersects(myViewRect, otherViewRect1) || Rect.intersects(myViewRect, otherViewRect2) || Rect.intersects(myViewRect, otherViewRect3) ||
+//                            Rect.intersects(myViewRect, otherViewRect4) || Rect.intersects(myViewRect, otherViewRect5) || Rect.intersects(myViewRect, otherViewRect6) || Rect.intersects(myViewRect, otherViewRect7)) {
+//
+//                        // animation.cancel();
+//                        dialog();
+//                        Toast.makeText(getApplicationContext(), "look it's toast", Toast.LENGTH_LONG).show();
+//                        moveLeft();
+//                    }
+               }
+
+            }
+            public void onAnimationRepeat(Animation anim) {
+
+            }
+            public void onAnimationEnd(Animation anim) {
+
+            }
+        });
+
+
     }//start()
+
+    public void moveLeft() {
+        playerImage.animate().translationXBy(-50f);
+
+    }
+
+    public void moveRight() {
+        playerImage.animate().translationXBy(50f);
+
+    }
+
+    public void dialog() {
+        //public void open(View view){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Use the arrow buttons to help the kitty avoid the raindrops!");
+                    alertDialogBuilder.setPositiveButton("Click to Play",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    start();
+                                }
+                            });
+
+//            alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int which) {
+//                    finish();
+//                }
+//            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+       // }
+    }
+
+    public void overlap(Animation animation) {
+        while(!(animation.hasEnded())) {
+            if (Rect.intersects(myViewRect, otherViewRect1) || Rect.intersects(myViewRect, otherViewRect2) || Rect.intersects(myViewRect, otherViewRect3) ||
+                    Rect.intersects(myViewRect, otherViewRect4) || Rect.intersects(myViewRect, otherViewRect5) || Rect.intersects(myViewRect, otherViewRect6) || Rect.intersects(myViewRect, otherViewRect7)) {
+
+                // animation.cancel();
+                dialog();
+                Toast.makeText(getApplicationContext(), "look it's toast", Toast.LENGTH_LONG).show();
+                moveLeft();
+            }
+        }
+    }
 
 }//mainActivity
